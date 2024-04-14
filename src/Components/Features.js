f4bfc7e953a6751b41464b6348906a800f7cb585
DavidOngora <ongoradavid5.gmail.com>
2024-04-14 17:43:13 +0300

    features component

---
+++ src/Components/Features.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '../Containers/Button'
+
+const Features = () => {
+  return (
+    <div className='grid mb-4'>
+      <span>Carlio Product Features</span>
+      <div className="grid text-3xl font-bold">
+      <span>The feature of Carlio engine</span>
+        <span>oil that you will benefit from</span> 
+      </div>
+      < Button description={"visit shop page"} />
+      
+      </div>
+  )
+}
+
+export default Features
