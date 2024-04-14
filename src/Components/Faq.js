abb2e35a529364d05efa5367b13242454b242e1d
DavidOngora <ongoradavid5.gmail.com>
2024-04-14 17:42:52 +0300

    frequently asked questions component

---
+++ src/Components/Faq.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import object from "../Object - Photo.png"
+const Faq = () => {
+  return (
+    <div className='flex mb-24'>
+      <img src={object} alt="" />
+      <div className="grid">
+      <span>FAQ Question</span>
+      <span>Frequently asked questions</span>
+      <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam adipisci repellendus aut?</span>
+      <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam adipisci repellendus aut?</span>
+      <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam adipisci repellendus aut?</span>
+      <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam adipisci repellendus aut?</span>
+      <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam adipisci repellendus aut?</span>
+      </div>
+    </div>
+  )
+}
+
+export default Faq
