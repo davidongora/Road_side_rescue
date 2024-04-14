c45ce7b7f992ad2c5738d2b2c95aec2d32f3bba0
DavidOngora <ongoradavid5.gmail.com>
2024-04-14 17:42:21 +0300

    brands component

---
+++ src/Components/Brands.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import audi  from "../BMW.png"
+// import Honda from "../Honda.png"
+// import Buggatti from "../Bugatti.png"
+// import audi from "../BMW.png"
+// import audi from "../BMW.png"
+// import audi from "../BMW.png"
+// import audi from "../BMW.png"
+// import audi from "../BMW.png"
+// import audi from "../BMW.png"
+// import audi from "../BMW.png"
+// import audi from "../BMW.png"
+// import audi from "../BMW.png"
+// import audi from "../BMW.png"
+
+const Brands = () => {
+  return (
+    <div>
+      <span>Brands Cars with Calio</span>
+      <div className="flex">
+      <img src={ audi } alt="" />
+      <img src={ audi } alt="" />
+      <img src={ audi } alt="" />
+      <img src={ audi } alt="" />
+      <img src={ audi } alt="" />
+      <img src={ audi } alt="" />
+      <img src={ audi } alt="" />
+      <img src={ audi } alt="" />
+      </div>
+      </div>
+  )
+}
+
+export default Brands
