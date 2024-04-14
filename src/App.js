61ee7832c6801b63d1c1b8cb632a7fe831d89fbe
DavidOngora <ongoradavid5.gmail.com>
2024-04-14 17:33:44 +0300

    project start file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PaymentForm from './Components/PaymentForm';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "./App.css"
+// import Navbar from './Components/Navbar'
+// import Footer from './Components/Footer'
+// import Home from './Components/Home'
+// import About from "./Components/About"
+// import Features from './Components/Features'
+// import Faq from './Components/Faq'
+// import Brands from './Components/Brands'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* < Navbar /> */}
+      {/* < Home />
+      < About />
+      < Features />
+      < Faq />
+      < Brands />
+      < Footer /> */}
+      <PaymentForm />
+      <ToastContainer />
     </div>
   );
 }
